8cb7acae3f5bbc6b7816b0b73f5ea2a816a58d99
Gui_Mello <89108147+GuiMello83@users.noreply.github.com>
2024-11-15 18:00:08 -0300

    Final I

--- Front/src/axiosConfig.js
+++ Front/src/axiosConfig.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://back-end-production-fcb2.up.railway.app', // URL do backend.
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'https://back-end-production-fcb2.up.railway.app',
+});
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `${token}`;
+  }
+  return config;
 });
 
 export default api;
